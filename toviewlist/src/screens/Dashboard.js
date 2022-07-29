2d4270c98260cbf94756dbd8dd091937765bf625
Gregor Pearce <gregorjpearce@gmail.com>
2022-07-29 18:37:02 +0100

    basic login/logout/sign up functionality

---
+++ toviewlist/src/screens/Dashboard.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Background from "../components/Background";
+import Logo from "../components/Logo";
+import Header from "../components/Header";
+import Paragraph from "../components/Paragraph";
+import Button from "../components/Button";
+
+export default function Dashboard({ navigation }) {
+  return (
+    <Background>
+      <Logo />
+      <Header>Your ToView List</Header>
+      <Paragraph>Here's what you've got to watch:</Paragraph>
+      <Button
+        mode="outlined"
+        onPress={() =>
+          navigation.reset({
+            index: 0,
+            routes: [{ name: "StartScreen" }],
+          })
+        }
+      >
+        Logout
+      </Button>
+    </Background>
+  );
+}
